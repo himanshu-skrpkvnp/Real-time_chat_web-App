9e21a0b579fbe48b2616e2ec21c03109a21c26e5
Himanshu Singh <himanshusinghskrpkvnp@Gmail.com>
2022-05-08 18:58:11 +0530

    first commit of project

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+const  chatForm = document.getElementById('chat-form') ;
+const chatMessages = document.querySelector('.chat-messages')
+const  roomName = document.getElementById('room-name');
+const  userList = document.getElementById('users') ; 
+
+
+// get username and room from URl
+const { username , room} = qs.parse(location.search , {
+    ignoreQueryPrefix : true 
+}) ;
+
+
+const socket = io();
+
+// join chatroom
+socket.emit( 'joinroom' , { username , room} ) ;
+
+//get room and users 
+socket.on('roomUsers' ,({ room , users }) =>{
+   outputRoomName( room ) ;
+   outputUsers(users) ;
+});
+
+
+
+// message from server
+socket.on( 'message' , message =>
+{
+   console.log(message) ;
+   outputMessage(message) ;
+
+
+   // scroll down every time new msg is there
+   chatMessages.scrollTop = chatMessages.scrollHeight ;
+});
+
+// message submit :
+chatForm.addEventListener('submit' ,(e)=>
+{
+    e.preventDefault() ;
+    //get message text
+    const msg = e.target.elements.msg.value ;
+
+
+    // Emit message to server
+    socket.emit('chatMessage' , msg) ;
+
+    // clear inputs once msg emitted 
+    e.target.elements.msg.value =  '' ;
+    e.target.elements.msg.focus() ;
+})
+
+// output message to DOM 
+function outputMessage(message)
+{
+    const div = document.createElement('div') ;
+    div.classList.add('message') ;
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
+    <p class="text">
+       ${message.text}
+    </p>` ;
+
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+//add roomname to DOM
+function outputRoomName(room)
+{
+   roomName.innerText = room ;
+}
+
+// add users to DOM
+function outputUsers(users)
+{
+    userList.innerHTML = `
+    ${users.map(user=>  `<li> ${user.username}</li>`).join('')}
+    ` ;
+}
+
+
+
---
+++ server.js
@@ -0,0 +1,79 @@
+const path = require("path") ;
+const express = require("express") ;
+const http = require('http') ;
+// const bodyParser = require("body-parser") ;
+const socketio = require('socket.io') ;
+const formatMessage = require('./utils/messages') ;
+const { userJoin , getCurrentUser , userLeave , getRoomUsers } = require('./utils/users') ;
+
+
+
+const app = express() ;
+const server = http.createServer(app) ;
+const io = socketio(server) ;
+//set static folder
+app.use(express.static(path.join(__dirname + "public" )));
+
+
+const botName = "ChatCord Bot" ;
+// run when clients connects 
+io.on( 'connection' , socket => 
+{    
+    socket.on( 'joinRoom' , ({ username , room }) =>{
+
+      const user = userJoin( socket.id , username , room  ) ;  
+       
+        // welcomes the current user
+    socket.emit('Messsage' , formatMessage( botName , "welcome to Let's Chat..!")) ;
+
+    // broadcast to all the user  of that room when a new  user connects 
+    socket.broadcast.to(user.room).emit('message' , formatMessage( botName ,`${user.username} has joined  the chat` )) ;
+
+    } )
+
+    // send users and room info
+    io.to(user.room).emit('roomUsers' , {
+       room : user.room ,
+       users : getRoomUsers(user.room)
+     });
+     
+
+
+    //listen for chat message 
+    socket.on('chatMessage' , (msg)=>{
+        
+        const user = getCurrentUser(socket.id) ;
+
+
+        io.to(user.room).emit('message' , formatMessage(  user.username , 'msg') );
+    })
+
+
+
+    // runs when clients disconnects
+    socket.on('disconnect' , ()=>
+    {
+        const user = userLeave(socket.id) ;
+        
+        io.to(user.room).emit('message' , formatMessage( botName ,`${user.username} has left the chat`)) ;
+
+    });
+
+    // send users and room info
+    io.to(user.room).emit('roomUsers' , {
+        room : user.room ,
+        users : getRoomUsers(user.room)
+      });
+      
+
+});
+
+
+
+const PORT = 3000 || process.env.PORT ;
+
+
+server.listen( PORT , ()=>console.log('server is running  at port ' + PORT ));
+
+// console.log('server is running  at port ' + PORT );
+
